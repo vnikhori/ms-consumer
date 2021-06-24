aca83841616dfff9973ee248982f11b24035f407
vnikhori <85665387+vnikhori@users.noreply.github.com>
2021-06-24 17:48:32 +0530

    Update MsConsumerApplication.java

--- src/main/java/com/poc/consumer/msconsumer/MsConsumerApplication.java
+++ src/main/java/com/poc/consumer/msconsumer/MsConsumerApplication.java
@@ -2,8 +2,12 @@ package com.poc.consumer.msconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@Configuration
+@EnableScheduling
 public class MsConsumerApplication {
 
 	public static void main(String[] args) {
