ac7066db9e5ab4b0821727a666499327eb05db7e
vnikhori <85665387+vnikhori@users.noreply.github.com>
2021-06-24 18:04:05 +0530

    Update MSConsumer.java

--- src/main/java/com/poc/consumer/msconsumer/resources/MSConsumer.java
+++ src/main/java/com/poc/consumer/msconsumer/resources/MSConsumer.java
@@ -1,12 +1,27 @@
 package com.poc.consumer.msconsumer.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.poc.consumer.msconsumer.service.ConsumerService;
 
+@Component
+@Configuration
 public class MSConsumer {	
 	
+	@Autowired
+	private ConsumerService service;
+	
+	
+	@Scheduled(fixedDelay = 60000)
+	public void startScheduler() {
+		System.out.println("starting......");
+		service.processData();
+		System.out.println("finished......");
+
+	}
 	
 	
 
