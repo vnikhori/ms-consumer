2c432fcf007961c08fac576e52ad8f3ca03ea5ed
vnikhori <85665387+vnikhori@users.noreply.github.com>
2021-06-24 16:08:59 +0530

    Update MSConsumer.java

--- src/main/java/com/poc/consumer/msconsumer/resources/MSConsumer.java
+++ src/main/java/com/poc/consumer/msconsumer/resources/MSConsumer.java
@@ -4,16 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
-@Configuration
+
 public class MSConsumer {	
 	
 	
-	@Scheduled(cron = "0 0/1 * 1/1 * ? *")	
-	public void startScheduler() {
-		
-		System.out.println("test");
-
-	}
+	
 
 }
