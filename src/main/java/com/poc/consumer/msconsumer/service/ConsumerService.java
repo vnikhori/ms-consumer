64d9964f17e1a776e80406de8b6121f033f3109a
vnikhori <85665387+vnikhori@users.noreply.github.com>
2021-06-25 12:38:35 +0530

    Update ConsumerService.java

--- src/main/java/com/poc/consumer/msconsumer/service/ConsumerService.java
+++ src/main/java/com/poc/consumer/msconsumer/service/ConsumerService.java
@@ -1,7 +1,7 @@
 package com.poc.consumer.msconsumer.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,33 +21,42 @@ public class ConsumerService {
 	@Autowired
 	private ConfirmationRepository confirmationRepo;
 	
-	
-	public void processData() {
-		List<Confirmation> confirmations = confirmationRepo.findAll();
-		List<String> ids = new ArrayList<>();		
+	/**
+	 * Get the completed confirmation and then save it to master data store
+	 */
+	public void processData() {		
+		List<Confirmation> confirmations = confirmationRepo.findConfirmationByStatus("COMPLETED");		
 		List<DataStore> data = consumerRepo.findAll();
-		for(DataStore one:data) {
-			ids.add(one.getId());
-		}
+		List<String> ids = data.stream().map(e -> e.getId()).collect(Collectors.toList());
 		for(Confirmation confirmation:confirmations) {
-			DataStore store = new DataStore(confirmation.getId(), confirmation.getRequestDateTime(), confirmation.getRequestMessageId(), confirmation.getCollectionUpdatedDateTime(), 
-					confirmation.getConfStatus(), confirmation.getConfirmationDateTime(), confirmation.getMessageDesc(), confirmation.getMessageStatusDateTime(), confirmation.getOptionalFields(),
-					confirmation.getOrigPaymentAmount(), confirmation.getOriginalValueDate(), confirmation.getPaymentAmount());
-			
-			if(ids!=null && ids.size()>0) {
-				if(!ids.contains(confirmation.getId())) {
-					if(store.getConfStatus().equalsIgnoreCase("COMPLETED")) {
-						consumerRepo.save(store);
-					}				
-				}
-			}else {
-				if(store.getConfStatus().equalsIgnoreCase("COMPLETED")) {
-					consumerRepo.save(store);
-				}	
-			}			
+			DataStore store = createDataStore(confirmation);			
+			if(ids.isEmpty()) {
+				consumerRepo.save(store);	
+			}
+			else if(!ids.contains(confirmation.getId())) {					
+				consumerRepo.save(store);				
+			}		
 			
 		}
 		
+	}
+
+
+	private DataStore createDataStore(Confirmation confirmation) {
+		DataStore store = new DataStore();
+		store.setCollectionUpdatedDateTime(confirmation.getCollectionUpdatedDateTime());
+		store.setConfirmationDateTime(confirmation.getConfirmationDateTime());
+		store.setConfStatus(confirmation.getConfStatus());
+		store.setId(confirmation.getId());
+		store.setMessageDesc(confirmation.getMessageDesc());
+		store.setMessageStatusDateTime(confirmation.getMessageStatusDateTime());
+		store.setOriginalValueDate(confirmation.getOriginalValueDate());
+		store.setOptionalFields(confirmation.getOptionalFields());
+		store.setOrigPaymentAmount(confirmation.getOrigPaymentAmount());
+		store.setPaymentAmount(confirmation.getPaymentAmount());
+		store.setRequestDateTime(confirmation.getRequestDateTime());
+		store.setRequestMessageId(confirmation.getRequestMessageId());
+		return store;
 	}
 
 }
