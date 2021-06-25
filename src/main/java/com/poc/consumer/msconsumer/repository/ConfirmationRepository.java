e3e4a18fdb03fb4550682f190fde01f06381eb8d
vnikhori <85665387+vnikhori@users.noreply.github.com>
2021-06-25 12:37:49 +0530

    Update ConfirmationRepository.java

--- src/main/java/com/poc/consumer/msconsumer/repository/ConfirmationRepository.java
+++ src/main/java/com/poc/consumer/msconsumer/repository/ConfirmationRepository.java
@@ -1,9 +1,15 @@
 package com.poc.consumer.msconsumer.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.poc.consumer.msconsumer.model.data.Confirmation;
 
 public interface ConfirmationRepository extends MongoRepository<Confirmation, Integer> {
+	
+	@Query(value = "{confStatus : ?0}")
+	List<Confirmation> findConfirmationByStatus(String confStatus);
 
 }
